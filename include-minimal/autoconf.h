ca14b0a938a1c9d4534a568efe365123ddecc06f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-09 18:27:44 +0200

    update build date and usage after recent changes
    Change-Id: Ia88a500235b20ad4908e5af88d85f588441efac7

--- include-minimal/autoconf.h
+++ include-minimal/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.20.1-cm9
  */
-#define AUTOCONF_TIMESTAMP "2012-05-28 03:24 +0200"
+#define AUTOCONF_TIMESTAMP "2012-07-09 18:28 +0200"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
