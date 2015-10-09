4dccac6ce2cbb24fc0c9a648940e0866c459a60b
Humberto Borba <humberos@gmail.com>
2015-10-09 16:12:59 -0300

    busybox: Fix build for android-6.0
    In file included from external/busybox/networking/route.c:44:0:
    external/busybox/networking/ipv6/ipv6_route.h:38:8: error: redefinition of 'struct in6_rtmsg'
     struct in6_rtmsg {
            ^
    In file included from bionic/libc/include/net/route.h:35:0,
                     from external/busybox/networking/route.c:37:
    bionic/libc/kernel/uapi/linux/ipv6_route.h:40:8: note: originally defined here
     struct in6_rtmsg {
            ^
    build/core/binary.mk:801: recipe for target '/android/omni-6.0/out/target/product/leo/obj/UTILITY_EXECUTABLES/static_busybox_intermediates/networking/route.o' failed
    make: *** [/android/omni-6.0/out/target/product/leo/obj/UTILITY_EXECUTABLES/static_busybox_intermediates/networking/route.o] Error 1
    make: *** Waiting for unfinished jobs....
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Iad9f517e3f5fc8f4316b6d42ed2ef465a0441dbc

--- networking/ipv6/ipv6_route.h
+++ networking/ipv6/ipv6_route.h
@@ -35,19 +35,6 @@
 #define RTF_LOCAL	0x80000000
 
 
-struct in6_rtmsg {
-	struct in6_addr		rtmsg_dst;
-	struct in6_addr		rtmsg_src;
-	struct in6_addr		rtmsg_gateway;
-	__u32			rtmsg_type;
-	__u16			rtmsg_dst_len;
-	__u16			rtmsg_src_len;
-	__u32			rtmsg_metric;
-	unsigned long		rtmsg_info;
-        __u32			rtmsg_flags;
-	int			rtmsg_ifindex;
-};
-
 #define RTMSG_NEWDEVICE		0x11
 #define RTMSG_DELDEVICE		0x12
 #define RTMSG_NEWROUTE		0x21
